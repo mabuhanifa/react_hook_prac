4117cbdd546e9539104a991a31cd9b250f4652ef
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-01 03:56:52 +0600

    updating

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,8 +1,15 @@
 import { useReducer, useState } from "react";
 
-const initialState = {
-  count: 0,
-};
+const initialState = [
+  {
+    id: Date.now(),
+    name: "John",
+    email: "john@example.com",
+  },
+];
+//   count: 0,
+//   users:
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
@@ -20,6 +27,8 @@ const reducer = (state, action) => {
         ...state,
         count: action.payload,
       };
+    case "add":
+      return [...state, action.payload];
 
     default:
       return state;
@@ -29,14 +38,76 @@ const reducer = (state, action) => {
 const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [value, setValue] = useState(null);
-  console.log(value);
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  console.log(state);
+  const submitForm = (e) => {
+    e.preventDefault();
+    setName("");
+    setEmail("");
+    const contact = {
+      id: Date.now(),
+      name,
+      email,
+    };
+    dispatch({ type: "add", payload: contact });
+  };
   return (
     <div style={{ margin: "100px" }}>
-      <h2>{state.count}</h2>
+      {/* <h2>{state.count}</h2> */}
       <button onClick={() => dispatch({ type: "increment" })}>Increase</button>
       <button onClick={() => dispatch({ type: "decrement" })}>Decrease</button>
       <input type="number" onChange={(e) => setValue(Number(e.target.value))} />
-      <button onClick={() => dispatch({ type: "byValue",payload:value })}>Add</button>
+      <button onClick={() => dispatch({ type: "byValue", payload: value })}>
+        Add
+      </button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "100px",
+          gap: "10px",
+        }}
+      >
+        <div>
+          <form onSubmit={submitForm}>
+            <input
+              type="text"
+              placeholder="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+          }}
+        >
+          {state.map((user) => {
+            return (
+              <ul style={{
+                border: "1px solid black",
+                padding: "10px",
+              }} key={user.id}>
+                <li>{user.name}</li>
+                <li>{user.email}</li>
+                <li>{user.id}</li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
