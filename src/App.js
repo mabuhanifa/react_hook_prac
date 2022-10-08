3afd3af72d9056ad61b4ee685884f716e5a3574e
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-08 16:14:03 +0600

    removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import Counter from "./components/counter/Counter";
-import Todo from "./components/todo/Todo";
 
 function App() {
   return (
-    <div style={{ margin: "100px" }}>
-      <Counter/>
-      <Todo />
+    <div>
+      
     </div>
   );
 }
