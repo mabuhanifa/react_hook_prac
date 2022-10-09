b1ba4a89fbe954b2986899b42d7184d479060df6
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-09 18:11:36 +0600

    removed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import Id from "./components/Id";
 
 function App() {
-  let createId = () => {
-    let s4 = () => {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    };
-    //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
-    return `${s4() + "-" + s4() + "-" + s4() + "-" + s4()}`;
-  };
-
-  let data = [];
-  for (let i = 0; i < 100; i++) {
-    data.push(createId());
-  }
-  const [arr, setArr] = useState(data);
+ 
   return (
     <div>
-      <div>
-        {arr.map((a) => {
-          return <Id key={a} aaa={a} arr={arr} setArr={setArr} />;
-        })}
-      </div>
+      
     </div>
   );
 }
