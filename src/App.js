4306c90c5d987090e84e9cdabac1d07abe7109b9
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-10 17:35:30 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const fetchProducts = async () => {
+    const {
+      data: { products },
+    } = await axios.get("https://dummyjson.com/products");
+    console.log(products);
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <div>
       <div></div>
