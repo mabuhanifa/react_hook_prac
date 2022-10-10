8f97adf39c24004ff4a3b76b40fd24b55e51f2b4
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-10 18:52:00 +0600

    reducer updated

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -2,6 +2,8 @@ export const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_PRODUCTS":
       return { ...state, products: action.payload };
+    case "ADD_TO_CART":
+      return { ...state, cart: [...state.cart, action.payload] };
     default:
       return state;
   }
