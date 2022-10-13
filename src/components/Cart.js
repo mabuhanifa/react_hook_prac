51ab6ca1dc6696dd9c7585941d143b8fdd9be7de
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-13 17:52:28 +0600

    cart updating

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
 
-const Cart = ({state,dispatch}) => {
-    const { cart } = state;
-    console.log(cart)
-    return (
-        <div className='cart'>
-            {cart?.length}
-        </div>
-    );
+const Cart = ({ state, dispatch }) => {
+  const { cart } = state;
+  const price = cart.reduce((a, c) => c.price + a, 0);
+  return (
+    <div className="cart">
+      <h2>Cart</h2>
+      <h4>Total Items: {cart?.length}</h4>
+      <h4>Total Price: {price}</h4>
+    </div>
+  );
 };
 
 export default Cart;
