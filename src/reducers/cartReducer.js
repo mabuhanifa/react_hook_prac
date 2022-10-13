c83a062f79300dcdbff7c6def40f03b69ee87529
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-13 18:56:22 +0600

    comments remove

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,17 +1,17 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    //"ADD_PRODUCTS"
-
     case "ADD_PRODUCTS":
       return { ...state, products: action.payload };
 
-    // "ADD_TO_CART"
     case "ADD_TO_CART":
       return { ...state, cart: [...state.cart, action.payload] };
 
-    // "REMOVE_FROM_CART"
     case "REMOVE_FROM_CART":
-      return { ...state, cart: state.cart.filter((c) => c.id !== action.payload.id) };
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+
     case "CHANGE_CART_QUANTITY":
       return {
         ...state,
